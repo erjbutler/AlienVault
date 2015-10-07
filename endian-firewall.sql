3f3515d6aff818d71389fb8cccc36059def27c19
erjbutler <erjbutler@users.noreply.github.com>
2015-10-07 11:21:57 -0700

    Update endian-firewall.sql

--- endian-firewall.sql
+++ endian-firewall.sql
@@ -7,4 +7,4 @@ DELETE FROM plugin_sid where plugin_id = "9456";
 INSERT IGNORE INTO plugin (id, type, name, description) VALUES (9456, 1, 'Endian', 'Endian Firewall');
 
 INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9456, 1, NULL, NULL, 'Firewall Traffic Allow', 1, 1);
-INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (4003, 2, NULL, NULL, 'Firewall Traffic Block', 1, 1);
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9456, 2, NULL, NULL, 'Firewall Traffic Block', 1, 1);
